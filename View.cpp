457240355eeda89765502025985aa15b7b8cfdc2
Dikiy-Yozhik <maxgarkaviy@gmail.com>
2025-03-06 22:45:03 +0300

    add UI

--- View.cpp
+++ View.cpp
@@ -1,16 +1,24 @@
 #include "view.h"
 
-void View::Print_World(Field& map)
-{
-    for(int i = 0; i < map.Get_rows(); i++){
-        for(int j = 0; j < map.Get_cols(); j++){
-            if(map.Get_Map()[i][j].Is_alive()){
-                cout << "l ";
+void View::CreateWindow(int width, int height) {
+    window.create(sf::VideoMode(width, height), "Game of Life");
+    window.setFramerateLimit(60);
+}
+
+void View::DrawWorld(sf::RenderWindow& window, Field& map) {
+    sf::RectangleShape cell(sf::Vector2f(10, 10)); // Размер клетки
+
+    for (int i = 0; i < map.Get_rows(); ++i) {
+        for (int j = 0; j < map.Get_cols(); ++j) {
+            if (map.Get_Map()[i][j].Is_alive()) {
+                cell.setFillColor(sf::Color::White); // Живая клетка
+            } else {
+                cell.setFillColor(sf::Color::Black); // Мертвая клетка
             }
-            else{
-                cout << "e ";
-            }
+
+            cell.setPosition(j * 10, i * 10); // Позиция клетки
+            window.draw(cell);
         }
-        cout << endl;
     }
 }
+
--- game.cpp
+++ game.cpp
@@ -1,59 +1,71 @@
 #include "game_logic.h"
 #include "view.h"
-
-#ifdef _WIN32
-#define CLEAR "cls"
-#else
-#define CLEAR "clear"
-#endif
-
-void Menu(int& act) {
-    cout << "Choose the action: " << endl
-         << "0 - Exit" << endl
-         << "1 - Continue" << endl
-         << "Your choice: ";
-
-    cin >> act;
-}
+#include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main() {
-    Game_Logic game_logic;
-    View view;
+    int width, height;
+    std::cout << "Enter width: ";
+    std::cin >> width;
+    std::cout << "Enter height: ";
+    std::cin >> height;
 
-    int act = 0;
-    int generation = 0;
+    Game_Logic game_logic(width, height);
 
-    while (1) {
-        //system(CLEAR); 
-        cout << "=== Generation " << generation << " ===" << endl;
-        view.Print_World(*game_logic.Get_Field());
-        cout << " =================== " << endl;
+    std::cout << "Enter coordinates of alive cells (x y). Enter -1 -1 to finish input." << std::endl;
 
-        Menu(act);
-        if (act == 0){
-            cout << "\nGame Over!" << endl;
-            break;
+    while (true) {
+        int x, y;
+        std::cout << "Enter coordinates (x y): ";
+        std::cin >> x >> y;
+
+        if (x == -1 && y == -1) break;
+
+        if (x >= 0 && x < width && y >= 0 && y < height) {
+            game_logic.Get_Field()->Get_Map()[y][x].Set_state(State_Cell::Alive);
+        } else {
+            std::cout << "Invalid coordinates. Please try again." << std::endl;
         }
-
-        generation++;
-        State_Game state = game_logic.Update();
-        if(state == State_Game::GameOver || state == State_Game::Stable){
-            //system(CLEAR);
-            if(state == State_Game::GameOver){
-                cout << "\nGame Over!" << endl;
-            } 
-            else if(state == State_Game::Stable){
-                cout << "\nStable configuration reached!" << endl;
-            }
-            break;
-        }
-
-        cout << endl;
     }
 
-    cout << " === Last generation " << generation << " ===" << endl;
-    view.Print_World(*game_logic.Get_Field());
-    cout << "=================" << endl;
+    View view;
+    view.CreateWindow(width * 15, height * 15 + 50); // Увеличение размера окна
+
+    int generation = 0;
+    bool gameOver = false;
+
+    while (view.GetWindow().isOpen()) {
+        sf::Event event;
+        while (view.GetWindow().pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                view.GetWindow().close();
+        }
+
+        if (!gameOver) {
+            view.GetWindow().clear();
+            view.DrawWorld(view.GetWindow(), *game_logic.Get_Field());
+            view.GetWindow().display();
+
+            State_Game state = game_logic.Update();
+            if (state == State_Game::GameOver || state == State_Game::Stable) {
+                gameOver = true;
+                if (state == State_Game::GameOver) {
+                    std::cout << "\nGame Over!" << std::endl;
+                } else if (state == State_Game::Stable) {
+                    std::cout << "\nStable configuration reached!" << std::endl;
+                }
+            }
+
+            generation++;
+            sf::sleep(sf::milliseconds(100)); // Задержка между кадрами
+        } else {
+            view.GetWindow().clear();
+            view.DrawWorld(view.GetWindow(), *game_logic.Get_Field());
+            view.GetWindow().display();
+
+            sf::sleep(sf::milliseconds(100)); // Маленькая задержка для обновления экрана
+        }
+    }
 
     return 0;
 }
